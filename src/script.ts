f0c2fb5ec725909d6c5b93ddef614ff4af65c979
”gabriellaberko” <75434964+gabriellaberko@users.noreply.github.com>
2025-10-15 08:20:25 +0200

    add weather icons from SMHI + clean code

--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,12 @@
 
 const url: string = `https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/18.062639/lat/59.329468/data.json`;
 
+// TO DO: change longitude & latitude (lon & lat) values to variables that we fetch from geoURL
+
 const filteredUrl: string = `https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/18.062639/lat/59.329468/data.json?timeseries=48&parameters=air_temperature,symbol_code`;
 
 const geoUrl: string = `https://wpt-a-tst.smhi.se/backend-startpage/geo/autocomplete/places/stockholm?pmponly=true`;
 
-// interface SunriseData {
-//   country: string
-//   sunrise: number
-//   sunset: number
-//   timezone: number
-// }
 
 
 interface fetchedDataStructure {
